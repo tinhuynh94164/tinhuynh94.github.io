bd466badc034559b54feb9341e934f17a381aaf6
tinhuynh <tinhuynh94164@gmail.com>
2016-12-09 09:48:27 +0700

    Update 6 bai HTML

---
+++ HTML-CSS/Baitap6/js/jsshowhide.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+	document.getElementById('arr_btn_1').style.background = "url(images/top-movie-trailers-arrows.gif) 0 -15px no-repeat";	
+	document.getElementById('section_acticle_2_wrapper1').style.display ="block";
+	document.getElementById('info_acticle1').style.display ="none";
+}
+
+function myFunction2() {
+	document.getElementById('arr_btn_2').style.background = "url(images/top-movie-trailers-arrows.gif) 0 8px no-repeat";	
+	document.getElementById('section_acticle_2_wrapper2').style.display ="block";
+	document.getElementById('info_acticle2').style.display ="block";
+}
+
+function myFunction3() {
+	document.getElementById('arr_btn_3').style.background = "url(images/top-movie-trailers-arrows.gif) 0 8px no-repeat";	
+	document.getElementById('section_acticle_2_wrapper3').style.display ="block";
+	document.getElementById('info_acticle3').style.display ="block";
+}
