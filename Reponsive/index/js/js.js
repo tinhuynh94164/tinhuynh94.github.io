25498d45f90ee87890bed639010e77701508510d
tinhuynh <tinhuynh94164@gmail.com>
2016-12-09 10:06:07 +0700

    Update baitap Resp

---
+++ Reponsive/index/js/js.js
@@ -0,0 +1,14 @@
+﻿window.onload=function() {
+	show();
+}
+var index=0;
+function show() {	
+	index++;	
+	document.images['image'].src='images/banner_'+index+'.jpg';	
+	setTimeout("show()", 3000);	
+	if(index==2) {
+		index=0;
+	}
+		
+}
+
